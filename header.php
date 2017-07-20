91e42d646c3b9ea7fe41c27fda49970d0617f48c
Andrew Hill <contact@andrewthomashill.co.uk>
2017-07-20 16:18:06 +0100

    Compatibility: enable support for random_bytes in PHP5

--- header.php
+++ header.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 session_start();
 if (empty($_SESSION['csrf_token'])) {
     $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
