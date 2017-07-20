f195a3d067e8e03e205587ba4f083ac51134e555
Andrew Hill <contact@andrewthomashill.co.uk>
2017-07-20 16:52:24 +0100

    Ensure dependencies are loaded before any PHP work begins

--- header.php
+++ header.php
@@ -1,17 +1,17 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+  ini_set('display_errors', 1);
+  ini_set('display_startup_errors', 1);
+  error_reporting(E_ALL);
 
-session_start();
-if (empty($_SESSION['csrf_token'])) {
-    $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
-}
+  include('includes/functions.php');
+  session_start();
+  if (empty($_SESSION['csrf_token'])) {
+      $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
+  }
 ?>
 <!DOCTYPE html>
 <html class="no-js" lang="">
 <head>
-  <?php include('includes/functions.php'); ?>
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
